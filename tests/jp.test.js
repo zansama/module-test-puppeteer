a7e65fc264fb992cfe7d61c4b0af70f603488ff8
zansama <clement.mortas@le-campus-numerique.fr>
2020-03-31 16:38:26 +0200

    afterall jp

--- tests/jp.test.js
+++ tests/jp.test.js
@@ -70,5 +70,8 @@ describe("Create a new user", () => {
         // ouvrir un onglet dans le navigateur
         page = await global.__BROWSER__.newPage()
     }, timeout)
-
+    afterAll(async () => {
+        // ouvrir un onglet dans le navigateur
+        await page.goto('http://polr.web-74.com/logout');
+    }, timeout)
 });
